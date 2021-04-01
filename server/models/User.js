54eac35e16bd6f5bfed7454c5c15ba633db3405e
James Manalang <james.e.manalang@gmail.com>
2021-04-02 01:03:23 +0800

    Install bcryptjs and validator. Hash user password before saving in the database

--- server/models/User.js
+++ server/models/User.js
@@ -1,4 +1,6 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
+import validator from 'validator';
 
 const Schema = mongoose.Schema;
 
@@ -15,18 +17,27 @@ const userSchema = new Schema({
 
   email: {
     type: String,
-    required: [true, 'Please provide an email'],
-    unique: true,
+    required: [true, 'User must have an email address'],
+    unique: [true, 'Email address is already taken'],
+    validate: [validator.isEmail, 'Please provide a valid email'],
   },
 
   password: {
     type: String,
     required: [true, 'Password is required'],
+    minlength: [8, 'Minimum of 8 characters'],
+    select: false,
   },
 
   confirmPassword: {
     type: String,
-    required: [true, 'Confirm password is required'],
+    required: [true, 'Please confirm your password'],
+    validate: {
+      validator: function (el) {
+        return el === this.password;
+      },
+      message: 'Passwords are not the same',
+    },
   },
 
   contacts: [
@@ -38,6 +49,19 @@ const userSchema = new Schema({
     },
   ],
 });
+
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  this.confirmPassword = undefined;
+  next();
+});
+
+userSchema.methods.correctPassword = async function (candidatePassword) {
+  return await bcrypt.compare(candidatePassword, this.password);
+};
 
 const User = mongoose.model('User', userSchema);
 
